795ea481feead2ff6606c2193878899d873fa27f
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-05-25 23:24:56 +0200

    Add debloated `libxml2`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,7 @@ else
 	PKG_TYPE='aarch64.pkg.tar.xz'
 fi
 
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
 
 echo "Installing build dependencies for sharun & AppImage integration..."
@@ -32,6 +33,7 @@ pacman -Syu --noconfirm \
 echo "Installing debloated pckages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$LLVM_URL" -O   ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O   ./libxml2-iculess.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
