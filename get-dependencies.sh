83185f54d3992d0780b3c04b549032cd010a0792
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-05-25 23:05:47 +0200

    Initial commit

---
+++ get-dependencies.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -eux
+
+sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+
+if [ "$(uname -m)" = 'x86_64' ]; then
+	PKG_TYPE='x86_64.pkg.tar.zst'
+else
+	PKG_TYPE='aarch64.pkg.tar.xz'
+fi
+
+LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
+
+echo "Installing build dependencies for sharun & AppImage integration..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel \
+	curl \
+	desktop-file-utils \
+	git \
+	libxtst \
+	wget \
+	xorg-server-xvfb \
+	zsync
+echo "Installing app dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	gnome-contacts \
+        mesa
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LLVM_URL" -O   ./llvm-libs.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+echo "All done!"
+echo "---------------------------------------------------------------"
---
+++ gnome-contacts-appimage.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+set -eu
+
+PACKAGE=gnome-contacts
+DESKTOP=org.gnome.Contacts
+ICON=org.gnome.Contacts.svg
+
+export ARCH="$(uname -m)"
+export APPIMAGE_EXTRACT_AND_RUN=1
+export VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
+echo "$VERSION" > ~/version
+
+UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+
+# Prepare AppDir
+mkdir -p ./AppDir/shared/lib
+cd ./AppDir
+
+cp -v /usr/share/applications/"$DESKTOP"            ./
+cp -v /usr/share/icons/hicolor/scalable/apps/"$ICON" ./
+cp -v /usr/share/icons/hicolor/scalable/apps/"$ICON" ./.DirIcon
+
+# ADD LIBRARIES
+wget "$SHARUN" -O ./sharun-aio
+chmod +x ./sharun-aio
+xvfb-run -a -- ./sharun-aio l -p -v -e -s -k \
+	/usr/bin/gnome-contacts*
+rm -f ./sharun-aio
+
+# Prepare sharun
+ln ./sharun ./AppRun
+./sharun -g
+
+# MAKE APPIMAGE WITH URUNTIME
+cd ..
+wget -q "$URUNTIME" -O ./uruntime
+chmod +x ./uruntime
+
+#Add update info to runtime
+echo "Adding update information \"$UPINFO\" to runtime..."
+./uruntime --appimage-addupdinfo "$UPINFO"
+
+echo "Generating AppImage..."
+./uruntime --appimage-mkdwarfs -f \
+	--set-owner 0 --set-group 0 \
+	--no-history --no-create-timestamp \
+	--compression zstd:level=22 -S26 -B8 \
+	--header uruntime \
+	-i ./AppDir -o "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+
+echo "Generating AppBundle..."
+wget -qO ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$(uname -m)" && chmod +x ./pelf
+echo "Generating [dwfs]AppBundle...(Go runtime)"
+./pelf --add-appdir ./AppDir \
+	    --appbundle-id="${PACKAGE}-${VERSION}" \
+     	    --compression "-C zstd:level=22 -S26 -B8" \
+	    --output-to "${PACKAGE}-${VERSION}-anylinux-${ARCH}.dwfs.AppBundle"
+
+echo "Generating zsync file..."
+zsyncmake *.AppImage -u *.AppImage
+zsyncmake *.AppBundle -u *.AppBundle
+
+echo "All Done!"
