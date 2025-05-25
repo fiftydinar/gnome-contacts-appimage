1fd8284f7bfbaa2019e92b94d274d0e78a85ca68
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-05-26 00:39:48 +0200

    Add debloated `mesa` & `opus`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,6 +10,8 @@ else
 	PKG_TYPE='aarch64.pkg.tar.xz'
 fi
 
+MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
 LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
 
@@ -34,6 +36,8 @@ echo "Installing debloated pckages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$LLVM_URL" -O   ./llvm-libs.pkg.tar.zst
 wget --retry-connrefused --tries=30 "$LIBXML_URL" -O   ./libxml2-iculess.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"        -O  ./opus-nano.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$MESA_URL"        -O  ./mesa.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
